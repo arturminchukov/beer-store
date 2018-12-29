ecda3882a16193e490dc52c890f6a37a8fd40566
Artur.Minchukou <artur_minchukov@mail.ru>
2018-12-29 12:51:36 +0300

    Add redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {applyMiddleware, compose, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {rootReducer} from './reducers/index';
+import App from './components/App/App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+function middleware({dispatch, getState}) {
+    return next => (action) => {
+        if (typeof action === 'function') {
+            return action(dispatch, getState);
+        }
+
+        return next(action);
+    };
+}
+
+const store = createStore(
+    rootReducer,
+    undefined,
+    composeEnhancers(applyMiddleware(middleware)),
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
