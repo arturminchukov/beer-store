a3bfb21245e2081807d7d99a86b3d0001f4ffc9a
Artur.Minchukou <artur_minchukov@mail.ru>
2019-01-18 17:30:53 +0300

    Moved logic from actions to reducers, changed structure of redux store

--- src/helper/beerArrayToObject.js
+++
@@ -1,9 +0,0 @@
-export default function beerArrayToObject(beerArray) {
-    let beerObject = {};
-
-    beerArray.forEach((beer) => {
-        beerObject[beer.id] = beer;
-    });
-
-    return beerObject;
-}
--- src/helper/filterFavoriteBeers.js
+++
@@ -1,5 +0,0 @@
-import {checkFavorite} from './checkFavorite';
-
-export default function filterFavoriteBeers(beers, favorites) {
-    return beers.map(beer => checkFavorite(beer, favorites));
-}
